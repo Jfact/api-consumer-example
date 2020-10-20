9ae2a38a33bdc82c00e71717971ac3b6f2e75923
jfact <jfact.den@gmail.com>
2020-10-20 15:31:24 +1300

    initial commit

--- projects/root/src/app/utils/b-utils.ts
+++ projects/root/src/app/utils/b-utils.ts
@@ -1,8 +1,3 @@
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl} from '@angular/forms';
 
 export type BFormControls<TModel> = {[Key in keyof TModel]: FormControl};
-export interface BFormComponent<TModel> {
-    form: FormGroup;
-    formName: string;
-    formControls: BFormControls<TModel>;
-}
